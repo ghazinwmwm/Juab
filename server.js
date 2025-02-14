a6064450a7c96ce1ab15e78c9ae5c09bf8523f64
ghazinwmwm <elashury@gmail.com>
2025-02-14 23:06:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = 1234
-const PAGE_ACCESS_TOKEN = ‏EAAJAmcUtD8MBO7l7xbmD1gPqkq2gdTTVLRCAQFyLZCBEloBiBZAo7Hmnrszv5dSj0tzFNw5TiwvCO6gmuwpMLdToyZCCk3EZCZARGZAJhI5FBCytwL4S2PoNljDZCXXfDZCsDZBsRRgCNeZBfkVQm5HLmgPXHXXZBthIgJZB7WOQenq7nPFRQ5Lv2uAOWHQnjCdviUgl77aY0UwSnLrLfDrauQZDZD
-const OPENAI_API_KEY = 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
+const PAGE_ACCESS_TOKEN = ‏"EAAJAmcUtD8MBO7l7xbmD1gPqkq2gdTTVLRCAQFyLZCBEloBiBZAo7Hmnrszv5dSj0tzFNw5TiwvCO6gmuwpMLdToyZCCk3EZCZARGZAJhI5FBCytwL4S2PoNljDZCXXfDZCsDZBsRRgCNeZBfkVQm5HLmgPXHXXZBthIgJZB7WOQenq7nPFRQ5Lv2uAOWHQnjCdviUgl77aY0UwSnLrLfDrauQZDZD";
+const OPENAI_API_KEY = "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";
 // تحقق من Webhook عند الربط
 app.get("/webhook", (req, res) => {
     let mode = req.query["hub.mode"];
